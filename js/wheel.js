d0017ad341121bc581b07ae5c703f7001f66b741
Юрий <htmlcoder001@gmail.com>
2024-02-06 16:48:16 +0300

    Base template

---
+++ js/wheel.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+   
+    // -------------spin-------------------
+    var resultWrapper = $('.spin-result-wrapper, .pop-up-window');
+    var wheel = $('.wheel-img');
+
+    $('.spin_active').on("click", function(event) {
+        $(this).off(event);
+        if (wheel.hasClass('rotated')) {
+            resultWrapper.css({
+                'display': 'block'
+            });
+        } else {
+            wheel.addClass('super-rotation');
+            setTimeout(function() {
+                resultWrapper.css({
+                    'display': 'block'
+                });
+            }, 8000);
+            setTimeout(function() {
+                $('.spin-wrapper').slideUp();
+                $('.order_block').slideDown();
+                start_timer();
+            }, 9500);
+            wheel.addClass('rotated');
+        }
+    });
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            resultWrapper.fadeOut();
+        }
+    });
+
+    $('.close-popup, .spin-result-wrapper').click(function() {
+        resultWrapper.fadeOut();
+    });
+});
